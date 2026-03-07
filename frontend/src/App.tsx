2a2eaf930bb9ae97090ff9e36f68c0d5ae089082
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-03-07 18:46:37 +0200

    feat: basic book viewer with navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import { walvis } from "./data/walvisInDeTuin";
+
 const App = () => {
+    const [currentSpread, setCurrentSpread] = useState(0);
+
+    const nextSpread = () => {
+        if (currentSpread < walvis.length - 1) {
+            setCurrentSpread((prev) => prev + 1);
+        }
+    };
+
+    const previousSpread = () => {
+        if (currentSpread > 0) {
+            setCurrentSpread((prev) => prev - 1);
+        }
+    };
+
     return (
-        <div>
-            <p>Hello, World!</p>
+        <div className="bg-black flex h-screen items-center justify-center w-screen">
+            <img
+                className="max-h-full object-contain"
+                key={currentSpread}
+                src={walvis[currentSpread]}
+            />
+            <div className="-translate-x-1/2 absolute bg-linear-to-r from-transparent inset-y-0 left-1/2 pointer-events-none to-transparent via-black/20 w-15 z-10" />
+
+            <div className="absolute flex inset-0">
+                <button className="h-full w-1/2" onClick={previousSpread} />
+                <button className="h-full w-1/2" onClick={nextSpread} />
+            </div>
         </div>
     );
 };
---
+++ frontend/src/data/walvisInDeTuin.ts
@@ -0,0 +1,27 @@
+import s1 from "../assets/books/walvis-in-de-tuin/spread_01.png";
+import s2 from "../assets/books/walvis-in-de-tuin/spread_02.png";
+import s3 from "../assets/books/walvis-in-de-tuin/spread_03.png";
+import s4 from "../assets/books/walvis-in-de-tuin/spread_04.png";
+import s5 from "../assets/books/walvis-in-de-tuin/spread_05.png";
+import s6 from "../assets/books/walvis-in-de-tuin/spread_06.png";
+import s7 from "../assets/books/walvis-in-de-tuin/spread_07.png";
+import s8 from "../assets/books/walvis-in-de-tuin/spread_08.png";
+import s9 from "../assets/books/walvis-in-de-tuin/spread_09.png";
+import s10 from "../assets/books/walvis-in-de-tuin/spread_10.png";
+import s11 from "../assets/books/walvis-in-de-tuin/spread_11.png";
+import s12 from "../assets/books/walvis-in-de-tuin/spread_12.png";
+
+export const walvis: string[] = [
+    s1,
+    s2,
+    s3,
+    s4,
+    s5,
+    s6,
+    s7,
+    s8,
+    s9,
+    s10,
+    s11,
+    s12
+];
