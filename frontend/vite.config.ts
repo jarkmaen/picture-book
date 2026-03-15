eed4c52e38f9e8466bea2d285645a42eaa096bb6
jarkmaen <73038801+jarkmaen@users.noreply.github.com>
2026-03-15 21:14:23 +0200

    config: remove trailing slash from vite base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/picture-book/",
+    base: "/picture-book",
     plugins: [react(), tailwindcss()]
 });
